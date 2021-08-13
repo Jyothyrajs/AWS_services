36fa5bc2dd297b44018ac84c557a9e51ef0fe4b6
jyothyraj <jyothyrajs@gmail.com>
2021-08-13 09:37:47 +0100

    Create bucket in the specified region

--- aws_create_s3.py
+++ aws_create_s3.py
@@ -1,9 +1,12 @@
 #!/bin/python3
 #Python Program for creating an S3 bucket
 import boto3
+import sys
 
 #connecting to S3
-myregion = 'eu-west-2'
+
+#myregion = 'eu-west-2'
+myregion = sys.argv[1]
 s3_client = boto3.client('s3',
 	region_name=myregion)
 
